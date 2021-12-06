c4511b47e999a010494fd9a31c61d0d18094c7b9
Vince van Noort <v.vannoort@egalit.nl>
2021-12-06 23:58:42 +0100

    disable dropdown of version for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,9 +57,9 @@ const config = {
             label: 'GitHub',
             position: 'right',
           },
-          {
-            type: 'docsVersionDropdown',
-          },
+          // {
+          //   type: 'docsVersionDropdown',
+          // },
         ],
       },
       footer: {
