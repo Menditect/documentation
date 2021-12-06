36b260e8ebae4e2d3973af84c77b58cf23335397
Vince van Noort <v.vannoort@egalit.nl>
2021-12-06 22:35:27 +0100

    set docs as landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // routeBasePath: '/',
           // Please change this to your repo.
@@ -66,7 +67,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
