8a9fcae44b6f427d040d082813ef9f37340a5064
Vince van Noort <v.vannoort@egalit.nl>
2022-01-24 16:04:24 +0100

    set to true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'menditect',
   projectName: 'documentation',
-  trailingSlash: false,
+  trailingSlash: true,
 
   presets: [
     [
