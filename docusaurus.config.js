03d1b700f07c67e8dcdfaf3ff7f9a92162db0c78
Vince van Noort <37207338+vincevannoort-egalit@users.noreply.github.com>
2022-04-01 15:46:07 +0200

    Update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Menditect Documentation',
   tagline: 'Go test it!',
   url: 'https://documentation.menditect.com/',
-  baseUrl: '/documentation/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
