b104a339ec91b3d417dd48a89685574954ebe1da
nielsagtereekmenditect <nielsagtereekmenditect@users.noreply.github.com>
2024-11-05 12:35:49 +0000

    deploy: 1a98b95d82fe47c3e9f7773441e314a278ef30fb

--- page-linker.js
+++ page-linker.js
@@ -2,6 +2,9 @@ function findCorrectPath(data, version, tag) {
   if (version && tag) {
     const versionLinks = data.versions[version];
     const path = versionLinks[tag];
+    if (path) return path;
+    versionLinks = data.versions['2.9'];
+    path = versionLinks[tag];
     return path;
   } else if (tag) {
     const path = data.nonVersions[tag];
